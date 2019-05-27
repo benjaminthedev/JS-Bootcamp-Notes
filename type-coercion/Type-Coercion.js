604f03ef15f4709203b5178ce8a93945fd418391
Benjamin Dordoigne <benjamin.d@onthegosystems.com>
2019-05-27 07:10:05 +0100

    Type Coercion

---
+++ type-coercion/Type-Coercion.js
@@ -0,0 +1,13 @@
+//Type Coercion String, Number, Boolean
+
+console.log('5' + 5)
+console.log('5' - 5)
+console.log('5' * 5)
+console.log('5' === 5)
+console.log('5' == 5)
+
+const values = true + 30
+const type = typeof value
+console.log(type)
+
+
