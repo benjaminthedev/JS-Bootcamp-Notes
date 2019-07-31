59d5b15b71a72050bc5319f3d2778ae2c93f9624
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-07-31 15:27:00 +0100

    updated notes

---
+++ testOOP/persons.js
@@ -0,0 +1,17 @@
+const Person = function (firstName, lastName, age, likes = []) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.likes = likes
+}
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age}.`
+    this.likes.forEach((like) => {
+        bio += ` ${this.firstName} likes ${like}.`
+    })
+    return bio
+}
+const me = new Person('Andrew', 'Mead', 27, ['Teaching', 'Biking'])
+console.log(me.getBio())
+const person2 = new Person('Clancey', 'Turner', 51)
+console.log(person2.getBio())
